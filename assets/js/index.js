c0e6dffa6fc792f1fe320bba39138e555da65e4b
sebasimm <sevastian.ignis@gmail.com>
2024-02-26 20:35:56 -0300

    feat(index): Add alert message and configure login form
    Details:
    - Added a dynamic alert message to display login results in the index.html.
    - Configured the login form to function properly.

---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+// live alert
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
+let wrapper = null;
+
+const appendAlert = (message, symbol, type) => {
+	wrapper = document.createElement('div');
+	wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible d-flex align-items-center" role="alert">`,
+        `<i class="fa-solid ${symbol} me-2"></i>`,
+        `   <div>${message}</div>`,
+		'</div>',
+	].join('');
+
+	alertPlaceholder.append(wrapper);
+};
+
+// sign in button
+$(document).ready(function () {
+	const correctUser = 'sebas';
+	const correctPass = '12345';
+
+	$('#signIn').click(function (event) {
+		event.preventDefault();
+
+		const username = $('#formName').val();
+		const password = $('#formPass').val();
+
+		if (username === correctUser && password === correctPass) {
+            appendAlert('Inicio de sesión exitoso', 'fa-circle-check', 'success');
+            setTimeout(function () {
+                
+                window.location.href = './assets/pages/menu.html';
+			}, 1500);
+		} else {
+            $('#signInForm').trigger('reset')
+            appendAlert('usuario o contraseña incorrecto', 'fa-triangle-exclamation', 'warning');
+            
+            setTimeout(function() {
+                wrapper.remove();
+            }, 1500);
+		}
+	});
+});
